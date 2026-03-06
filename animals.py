284461f8049a65f9b3e751141cccb2d5cc6acbbd
Mortymier <sanielmg@gmail.com>
2026-03-06 20:50:04 +0800

    Add initial list of animals for dropdown menu

---
+++ animals.py
@@ -0,0 +1,23 @@
+# File containing list of animals found in the dropdown menus
+
+animals = [
+    "Anaconda",
+    "Badger",
+    "Bear",
+    "Cheetah",
+    "Crocodile",
+    "Eagle",
+    "Elephant",
+    "Gorilla",
+    "Hippopotamus",
+    "Jaguar",
+    "Komodo Dragon",
+    "Lion",
+    "Python",
+    "Rhinoceros",
+    "Shark",
+    "Snake",
+    "Tiger",
+    "Wild Boar",
+    "Wolf",
+]
